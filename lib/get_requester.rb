c33be857dda7f6fd645c1e72beeee76a0b30d39b
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-20 23:36:12 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,17 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  attr_accessor :url
+  def initialize (name)
+    @url = name
+  end
+  
+  def get_response_body
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  
+end
